a0aeb175f7a3b669173702ac7a360e6a6ac4e6b3
Muhammad Athallah Zhuhry <26764804+muhammadzhuhry@users.noreply.github.com>
2026-04-20 21:35:52 +0700

    Add: python-variable-types - submission-0

---
+++ Python For Beginners/python-variable-types/submission-0.py
@@ -0,0 +1,13 @@
+integer_type = 10
+float_type = 99.5
+bool_type = True
+string_type = "Homeless Studio"
+list_type = [1,2,3]
+
+
+
+print(type(integer_type))
+print(type(float_type))
+print(type(bool_type))
+print(type(string_type))
+print(type(list_type))
